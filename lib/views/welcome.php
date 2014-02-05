a200bcab4f2e1d53fc58c6dacbb61b5b326bd8b2
rlemon <rob.lemon@gmail.com>
2014-02-04 19:21:25 -0500

    adding base

---
+++ lib/views/welcome.php
@@ -0,0 +1,31 @@
+<div id="container">
+	<div id="primary-content" role="main">
+		<div id="unit-img-content" class="unit">
+			<header id="header">
+			<h1>place anything</h1>
+			</header>
+			<p>
+				<strong>A quick and simple service for getting pictures for use as placeholders in your designs or code.</strong> Just put your image size (width <span class="amp">&amp;</span> height) after our URL and you'll get a&nbsp;placeholder.
+			</p>
+			<p class="links">
+				enter links like this: http://example.com/i/400/600 <br>
+				or http://example.com/image/400/600 
+			</p>
+			<img id="image-0" src="i/408/287" alt=""/>
+		</div>
+		<div id="unit-many-many-pictures-of-small-imgs" class="unit">
+			<img id="image-1" src="i/200/287?1" alt=""/>
+			<img id="image-2" src="i/200/140?2" alt=""/>
+			<img id="image-3" src="i/200/139?3" alt=""/>
+			<img id="image-4" src="i/200/286?4" alt=""/>
+			<img id="image-5" src="i/96/140?5" alt=""/>
+			<img id="image-6" src="i/96/139?6" alt=""/>
+			<img id="image-7" src="i/200/138?7" alt=""/>
+		</div>
+	</div>
+	<footer id="footer" role="contentinfo">
+	<p>
+		Developed by <a href="http://github.com/rlemonrlemon" target="_blank">rlemon</a>
+	</p>
+	</footer>
+</div>
